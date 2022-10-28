f942d9ff9eca22ffef49dda0b7ba5219d4e74237
Home_Laptop <Jimmlius923@gmail.com>
2022-10-28 21:20:14 +0100

    Done with Project

--- musicapp/models.py
+++ musicapp/models.py
@@ -7,15 +7,22 @@ class Artiste(models.Model):
     last_name = models.CharField(max_length=200)
     age = models.IntegerField()
 
-    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
 
 class Song(models.Model):
     title = models.CharField(max_length=400)
     date_released = models.DateField(default=datetime.today)
     likes = models.IntegerField()
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
     
 
 class Lyric(models.Model):
     content = models.TextField()
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
